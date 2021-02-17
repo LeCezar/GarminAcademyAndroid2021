5d88aed8b976dae2e0462a1b035bbd98d9c2c9d2
LeCezar <lecezar2014@gmail.com>
2021-02-17 13:30:38 +0200

    Adds the part 2 of the GarminKaptain application from the course.

--- Homework/AndroidBasics/GarminKaptain2021/app/src/main/java/com/garmin/garminkaptain/ui/PoiDetailsFragment.kt
+++ Homework/AndroidBasics/GarminKaptain2021/app/src/main/java/com/garmin/garminkaptain/ui/PoiDetailsFragment.kt
@@ -10,13 +10,14 @@ import android.widget.Button
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.garmin.garminkaptain.R
 import com.garmin.garminkaptain.TAG
 import com.garmin.garminkaptain.data.poiList
 
 class PoiDetailsFragment : Fragment() {
 
-    private val poi = poiList.first()
+    private val args: PoiDetailsFragmentArgs by navArgs()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -39,14 +40,20 @@ class PoiDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d(TAG, "onViewCreated: called")
-        view.apply {
-            findViewById<TextView>(R.id.poi_name_view).text = poi.name
-            findViewById<TextView>(R.id.poi_type_view).text = poi.poiType
-            findViewById<RatingBar>(R.id.poi_rating_view).rating = poi.reviewSummary.averageRating.toFloat()
-            findViewById<TextView>(R.id.poi_num_reviews_view).text =
-                getString(R.string.label_num_reviews, poi.reviewSummary.numberOfReviews)
-            findViewById<Button>(R.id.poi_view_reviews_button).isEnabled =
-                poi.reviewSummary.numberOfReviews > 0
+
+        val poiId = args.poiId
+        val poi = poiList.find { it.id == poiId }
+        poi?.let {
+            view.apply {
+                findViewById<TextView>(R.id.poi_name_view).text = poi.name
+                findViewById<TextView>(R.id.poi_type_view).text = poi.poiType
+                findViewById<RatingBar>(R.id.poi_rating_view).rating =
+                    poi.reviewSummary.averageRating.toFloat()
+                findViewById<TextView>(R.id.poi_num_reviews_view).text =
+                    getString(R.string.label_num_reviews, poi.reviewSummary.numberOfReviews)
+                findViewById<Button>(R.id.poi_view_reviews_button).isEnabled =
+                    poi.reviewSummary.numberOfReviews > 0
+            }
         }
     }
 
